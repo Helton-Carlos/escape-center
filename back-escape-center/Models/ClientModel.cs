c72d44a91bf3560c5f5ef916f29af3064ed58b92
Helton-Carlos <heltoncarlospa>
2026-06-26 22:00:44 -0300

    feat: add client model

---
+++ back-escape-center/Models/ClientModel.cs
@@ -0,0 +1,11 @@
+namespace back_escape_center.Models;
+
+public class ClientModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+    public DateTime DataNascimento { get; set; }
+    public DateTime DataCadastro { get; set; } = DateTime.Now;
+}
