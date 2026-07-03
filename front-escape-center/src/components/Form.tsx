cd7f3f50addd904a199f0af6c203463a6ee743b8
Helton Silva <helton.silva@melhorenvio.com>
2026-07-03 14:42:05 -0300

    feat: components form

--- front-escape-center/src/components/Form.tsx
+++ front-escape-center/src/components/Form.tsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function Form() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if(email && password) {
+      navigate('/'); 
+    } 
+  }
   return (
-    <form className="bg-gray-800 w-90 px-4 py-4 flex flex-col gap-5 mx-auto">
+    <form className="bg-gray-800 w-90 px-4 py-4 flex flex-col gap-5 mx-auto" onSubmit={handleSubmit}>
       <h1 className="text-2xl text-center font-bold mt-2">
         Escape Center
       </h1>
@@ -11,6 +25,8 @@ export function Form() {
         </label>
 
         <input 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           type="e-mail" 
           placeholder="e-mail" 
           className="input mt-2"
@@ -23,14 +39,23 @@ export function Form() {
         </label>
 
         <input 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           type="password" 
           placeholder="*******" 
           className="input mt-2"
         />
       </div>
 
-      <button className="btn btn-primary">Login</button>
-      <button className="btn btn-link text-white">Cadastrar</button>
+      <div className="flex flex-col gap-2">
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+
+        <button className="btn btn-link text-white" type="button">
+          Cadastrar
+        </button>
+      </div>
     </form>  
   )
 }
