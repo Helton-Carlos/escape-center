0c5c8b6ae277dea5e0072aee7a9670c9aa122274
Helton Silva <helton.silva@melhorenvio.com>
2026-07-03 12:44:38 -0300

    feat: add tailwind

--- front-escape-center/src/App.tsx
+++ front-escape-center/src/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
+import { Dashboard } from '@/views/Dashboard'
 import { Login } from '@/views/Login'
 
 function App() {
   return (
     <>
      <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
      </Routes>
     </>
