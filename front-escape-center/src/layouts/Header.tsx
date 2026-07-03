6362e6e70f07f886bdfa5d0ab46dda5f5217dbc9
Helton Silva <helton.silva@melhorenvio.com>
2026-07-03 16:49:30 -0300

    feat: add sidebar

--- front-escape-center/src/layouts/Header.tsx
+++ front-escape-center/src/layouts/Header.tsx
@@ -2,19 +2,44 @@ import { Link } from 'react-router-dom'
 
 export function Header() {
   return (
-    <header className="w-full h-20 bg-gray-800 flex items-center justify-center">
-      <div className="w-full max-w-7xl flex items-center justify-between px-4">
-        <h1 className="text-2xl">Escape Center</h1>
+    <header className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label htmlFor="my-drawer-1" className="btn drawer-button">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+          </label>
 
-        <nav className="flex items-center gap-4">
-          <ul>
-            <li>
-              <Link to="/login" className="text-white hover:text-gray-400">
-                Sair
-              </Link>
-            </li>
-          </ul>
-        </nav>
+          <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
+        
+           <div className="drawer-side">
+            <label 
+              htmlFor="my-drawer-1"
+              aria-label="close sidebar" 
+              className="drawer-overlay"
+            />
+
+            <ul className="menu bg-base-200 min-h-full w-80 p-4">
+              <li><Link to="/">Dashboard</Link></li>
+              <li><Link to="/login">Login</Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="navbar-center">
+        <Link to="/" className="btn btn-ghost text-xl">Escape Center</Link>
+      </div>
+      
+      <div className="navbar-end">
+        <button className="btn btn-ghost btn-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
+        </button>
+        <button className="btn btn-ghost btn-circle">
+          <div className="indicator">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
+            <span className="badge badge-xs badge-primary indicator-item"></span>
+          </div>
+        </button>
       </div>
     </header>
   )
