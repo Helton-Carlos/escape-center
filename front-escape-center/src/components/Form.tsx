6aec18b266e1faa059f8fbb219b3eeccf7c2da6b
Helton Silva <helton.silva@melhorenvio.com>
2026-07-03 14:37:10 -0300

    feat: add page form

--- front-escape-center/src/components/Form.tsx
+++ front-escape-center/src/components/Form.tsx
@@ -1,7 +1,36 @@
 export function Form() {
   return (
-    <div>
-      <h1>Form</h1>
-    </div>  
+    <form className="bg-gray-800 w-90 px-4 py-4 flex flex-col gap-5 mx-auto">
+      <h1 className="text-2xl text-center font-bold mt-2">
+        Escape Center
+      </h1>
+
+      <div>
+        <label htmlFor="email" className="text-white">
+          E-mail
+        </label>
+
+        <input 
+          type="e-mail" 
+          placeholder="e-mail" 
+          className="input mt-2"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password" className="text-white">
+          Senha
+        </label>
+
+        <input 
+          type="password" 
+          placeholder="*******" 
+          className="input mt-2"
+        />
+      </div>
+
+      <button className="btn btn-primary">Login</button>
+      <button className="btn btn-link text-white">Cadastrar</button>
+    </form>  
   )
 }
--- front-escape-center/src/views/Login.tsx
+++ front-escape-center/src/views/Login.tsx
@@ -1,7 +1,9 @@
+import { Form } from "@/components/Form";
+
 export function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Form />
     </div>
   )
 }
