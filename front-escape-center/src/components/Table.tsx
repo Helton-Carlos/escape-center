aa3c7768fe216beeff8cd8ff2fa4f112928a7a07
Helton Silva <helton.silva@melhorenvio.com>
2026-07-15 09:28:51 -0300

    feat: add table

---
+++ front-escape-center/src/components/Table.tsx
@@ -0,0 +1,26 @@
+export function Table({ columns, data }: { columns: string[], data: { id: string, cliente: string, servico: string, status: string }[] }) {
+  return (
+    <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 mt-8">
+      <table className="table">
+        <thead>
+          <tr>
+            {columns.map((column, index) => (
+              <th key={index}>{column}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+        {
+          data.map((row, index) => (
+            <tr key={index}>
+              <td>{row.id}</td>
+              <td>{row.cliente}</td>
+              <td>{row.servico}</td>
+              <td>{row.status}</td>
+            </tr>
+          ))
+        }
+        </tbody>
+      </table>
+    </div>
+)}
--- front-escape-center/src/views/Dashboard.tsx
+++ front-escape-center/src/views/Dashboard.tsx
@@ -1,7 +1,24 @@
 import { Card } from "@/components/Card";
+import { Table } from "@/components/Table";
 import { CardInfo } from "@/utils/Home";
 
 export function Dashboard() {
+  const columns = ['','Cliente', 'Serviço', 'Status'];
+  const rows = [
+    {
+      id: '1',
+      cliente: 'John Doe',
+      servico: 'Revisão',
+      status: 'Em andamento'
+    },
+    {
+      id: '2',
+      cliente: 'Jane Smith',
+      servico: 'Escapamento',
+      status: 'Concluído'
+    }
+  ]
+
   return (
     <div className="p-4 max-w-7xl mx-auto w-full">
       <h1 className="text-2xl my-4">Dashboard</h1>
@@ -16,6 +33,11 @@ export function Dashboard() {
           />
         ))}
       </div>
+
+      <Table 
+        columns={columns} 
+        data={rows}
+      />
     </div>
   )
 }
